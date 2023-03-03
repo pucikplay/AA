79b6631cdffdf3461e77dc036bd2c2e21690273e
Gabriel <pucikplay@gmail.com>
2023-03-03 17:55:04 +0100

    First scenario

---
+++ Laby/L1/z1_a.py
@@ -0,0 +1,26 @@
+import random
+import matplotlib.pyplot as plt
+import math
+
+no_tests = 100
+n = int(input("Enter the number of devices: "))
+devices = [0] * n
+probs = [pow(2,b) for b in range(int(-1.5*math.log(n,2)),int(-0.75*math.log(n,2)))]
+averages = []
+
+for p in probs:
+    print(p)
+    rounds = []
+    for i in range(0,no_tests):
+        no_rounds = 0
+        while sum(devices) != 1:
+            devices = [(0 if random.random() > p else 1) for i in range(0,n)]
+            no_rounds += 1
+        rounds.append(no_rounds)
+        devices = [0] * n
+
+    average = sum(rounds) / len(rounds)
+    averages.append(average)
+
+plt.plot(probs, averages)
+plt.show()
