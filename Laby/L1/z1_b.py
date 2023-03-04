b4c9e85c25a80de96a2deaa219b19eef33fb52b3
Gabriel <pucikplay@gmail.com>
2023-03-04 09:51:23 +0100

    Second scenario

--- Laby/L1/z1_b.py
+++ Laby/L1/z1_b.py
@@ -1,20 +1,30 @@
 import random
 import matplotlib.pyplot as plt
 import math
+import time
 
 no_tests = 50
 n = 1000
+u_values = [1000 * b for b in range(1,20)]
 devices = [0] * n
-probs = [pow(2,b) for b in range(int(-1.5*math.log(n,2)),int(-0.75*math.log(n,2)))]
 averages = []
 
-for p in probs:
-    print(p)
+for u in u_values:
+    print(u)
     rounds = []
+    L = math.ceil(math.log(u,2))
     for i in range(0,no_tests):
+        p = 0.5
         no_rounds = 0
+        counter = 0
         while sum(devices) != 1:
             devices = [(0 if random.random() > p else 1) for i in range(0,n)]
+            if counter > L:
+                p = 0.5
+                counter = 0
+            else:
+                p *= 0.5
+            counter += 1
             no_rounds += 1
         rounds.append(no_rounds)
         devices = [0] * n
@@ -22,5 +32,5 @@ for p in probs:
     average = sum(rounds) / len(rounds)
     averages.append(average)
 
-plt.plot(probs, averages)
+plt.plot(u_values, averages)
 plt.show()
